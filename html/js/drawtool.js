c1757ba4b5fae5415eab309efb064b8f0a7fd5b9
nrmnr <n.pujita+github@gmail.com>
2014-08-18 00:56:46 +0900

    サイズ初期化処理

--- html/js/drawtool.js
+++ html/js/drawtool.js
@@ -1,3 +1,21 @@
 $(
 function(){
+
+  var DrawTool = function($drawcanvas){
+    var resize_stage = function(){
+      var w = $(window).width();
+      var h = $(window).height();
+      $drawcanvas.width(w-2);
+      $drawcanvas.height(h-2-52);
+    };
+
+    var initialize = function(){
+      $(window).on('resize', resize_stage);
+      resize_stage();
+    };
+
+    initialize();
+  };
+
+  new DrawTool($('#drawtool'));
 });
